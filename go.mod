c141aa46f2dbbe2ec8ec030600780c9d07b8ba61
Ezkrraa <1052307@hr.nl>
2023-05-31 15:59:54 +0200

    Autoload test

--- go.mod
+++ go.mod
@@ -4,9 +4,13 @@ go 1.20
 
 require (
 	github.com/bwmarrin/discordgo v0.27.1 // direct
+	golang.org/x/text v0.9.0 // direct
+)
+
+require (
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jpfuentes2/go-env v0.0.0-20150316001728-8e0a68de05f2 // indirect
 	// github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.9.0 // direct
 )
--- main.go
+++ main.go
@@ -9,15 +9,19 @@ import (
 	"discord-weather-bot/bot"
 	"log"
 	"os"
+
+	_ "github.com/jpfuentes2/go-env/autoload"
 )
 
 func main() {
+
 	if _, err := os.Stat(".env"); err != nil {
 		file, err2 := os.Create(".env")
 		if err2 != nil {
 			log.Fatal("Failed to make an empty file, try adding .env to source")
 		}
 		file.WriteString("export BOT_TOKEN=<token here>\nexport OPENWEATHER_TOKEN=<token here>")
+		file.Close()
 	}
 	botToken, ok := os.LookupEnv("BOT_TOKEN")
 	if !ok {
@@ -31,5 +35,4 @@ func main() {
 	bot.BotToken = botToken
 	bot.OpenWeatherToken = openWeatherToken
 	bot.Run()
-
 }
